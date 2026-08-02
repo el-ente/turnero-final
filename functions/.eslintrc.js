7ad9432520218775739b0f158eb4ed0546af61b1
Franco Valledor <francovalledor@gmail.com>
2026-08-02 16:37:36 -0300

    fix: exclude functions/scripts from type-aware eslint parsing

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     "/lib/**/*", // Ignore built files.
     "/generated/**/*", // Ignore generated files.
     "jest.config.js", // Not included in tsconfig, type-aware lint can't parse it.
+    "scripts/**/*.js", // Plain Node deploy-time scripts, not part of the TS project.
   ],
   plugins: [
     "@typescript-eslint",
